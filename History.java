8c5b0e3513a4b7885dc822daf96254443c6aea80
Andrei Onea <andreionea25@gmail.com>
2018-03-23 09:02:43 +0200

    All classes

---
+++ History.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class History {
+
+  public history;
+
+    public Vector  myShippment;
+    public Shippment myShippment;
+    public Vector  myStatus;
+
+  public void changeStatus( Status) {
+  }
+
+  public void getStatus( Status) {
+  }
+
+}
---
+++ Status.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class Status {
+
+  public IN_STORAGE;
+
+  public ON_ROUTE;
+
+  public CURIER_STORAGE;
+
+  public SHIPPED;
+
+    public Vector  myHistory;
+
+}
